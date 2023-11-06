3947d4e775e73130bdbf6d07b0408f18d12d5f8a
lekhanamaridi <lekhanamaridi444@gmail>
2023-11-06 12:53:50 +0530

    edited

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,10 @@ import swami from './images/swami.jpg';
 import clg3 from './images/college3.jpg';
 import clg4 from './images/college4.jpg';
 import clg5 from './images/college5.jpg';
+import clg6 from './images/event1.jpg';
+import clg7 from './images/event3.jpeg';
+import clg8 from './images/event2.jpeg';
+import clg9 from './images/event4.jpg';
 import './Footer.css';
 function Footer()
 {
@@ -30,13 +34,10 @@ function Footer()
             </div>
             <div>
                 <h1 className='styling1'>A Commitment to Technical Education</h1>
-                <div className='a2'>
-                    <p className='text'>The real catalyst, which plays important role in shaping the career of a student, is the place of study and its surroundings. Infrastructure of STJ Institute of Technology is at a serene environment on the outskirts of Ranebennur attracts one and all. The campus of the institute extends over an area of 52 acres of land, specially selected to keep the students away from the vitiated atmosphere of city life. Campus is extensively landscaped with green environment. Established in the year 1980. The college is affiliated to Visveswaraiah Technological University, Approved by All India Council for Technical Education and by the Government of Karnataka. Ranebennur is situated in Haveri District of Karnataka, on Poona Bangalore National Highway (NH-4). 295km from Bangalore and 100km from Dharwad. It can be easily reached either by Road or by Rail. Bus frequency from Bangalore to Ranebennur is very high. The Institute is situated at 3km from the Ranebennur Bus Station. The climate throughout the year is mild and salubrious.</p>
-                </div>
-                <div  className='image6'>
-                    <img src={clg5}></img>
-                    <h2 className='text2'> A Dream Place to Build Your <br></br>Career.....</h2>
-                </div>
+                <div className='align'>
+                        <p className='text'>The real catalyst, which plays important role in shaping the career of a student, is the place of study and its surroundings. Infrastructure of STJ Institute of Technology is at a serene environment on the outskirts of Ranebennur attracts one and all. The campus of the institute extends over an area of 52 acres of land, specially selected to keep the students away from the vitiated atmosphere of city life. Campus is extensively landscaped with green environment. Established in the year 1980. The college is affiliated to Visveswaraiah Technological University, Approved by All India Council for Technical Education and by the Government of Karnataka. Ranebennur is situated in Haveri District of Karnataka, on Poona Bangalore National Highway (NH-4). 295km from Bangalore and 100km from Dharwad. It can be easily reached either by Road or by Rail. Bus frequency from Bangalore to Ranebennur is very high. The Institute is situated at 3km from the Ranebennur Bus Station. The climate throughout the year is mild and salubrious.</p>
+                        <img src={clg5} className='image6'></img>
+                </div>    
             </div>
             <div>
                 <h1 className='styling2'>Vision</h1>
@@ -49,11 +50,58 @@ function Footer()
             <br></br>
             <div>
                 <h2 className='styling1'>Our Strength</h2>
-                <div className='text3'>
-                    <h1 className='text4'>6<br></br>UG Programmes</h1>
-                    <h1 className='text5'>2000<br></br>Students</h1>
-                    <h1 className='text6'>135<br></br>Teachers</h1>
-                    <h1 className='text7'>186+<br></br>Placements</h1>
+                <div className='place'>
+                    <div className='text3'>
+                        <h1>6<br></br>UG Programmes</h1>
+                    </div>
+                    <div className='text3'>
+                        <h1>2000<br></br>Students</h1>
+                    </div>
+                    <div className='text3'>
+                        <h1>135<br></br>Teachers</h1>
+                    </div>
+                    <div className='text3'>
+                        <h1>186+<br></br>Placements</h1>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <h1 className='styling1'>Latest Events and News</h1>
+                </div>
+                <div>
+                    <p className='l1'><b><i>Three days Workshop from 14thto 16th June</i></b></p>
+                    <div className='l2'>
+                        <p>Department of Computer Science & Engineering conducted three days workshop on Android application Development  <br></br>and Python Programming from 14th to 16th June 2023.</p>
+                        <img src={clg6} className='l3'></img>
+                    </div>
+                </div>
+                <hr></hr>
+                <hr></hr>
+                <div>
+                    <p className='l1'><b><i>World Environment Day Celebration</i></b></p>
+                    <div className='l2'>
+                        <p>NSS Unit of our Institute celebrated World Environment Day on 5thJune 2023 in our campus.</p>
+                        <img src={clg7} className='l3'></img>
+                    </div>
+                </div>
+                <hr></hr>
+                <hr></hr>
+                <div>
+                    <p className='l1'><b><i>Kannada Rajyotsava Celebration</i></b></p>
+                    <div className='l2'>
+                        <p>Kannada Rajyotsava was celebrated on 26th Nov 2022 at STJ Institute of Technology.</p>
+                        <img src={clg8} className='l3'></img>
+                    </div>
+                </div>
+                <hr></hr>
+                <hr></hr>
+                <div>
+                    <p className='l1'><b><i>Secura 2k23 : Sports & Extra Curricular Activities</i></b></p>
+                    <div className='l2'>
+                        <p>Annual day celebrated on 31-05-2023 at STJ Institute of Technology.</p>
+                        <img src={clg9} className='l3'></img>
+                    </div>
                 </div>
             </div>
         </div>
